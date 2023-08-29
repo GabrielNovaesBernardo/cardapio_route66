70bd1e6f949d7cc1d4426284de92f3e2602184d2
Gabriel Novaes <gabrielnovaisoficial@gmail.com>
2023-08-29 12:52:52 -0300

    29/08

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function menu(){
     document.getElementById('menu1').style.backgroundColor = 'red'
 
 }
+
+function carhambuerguerid(){
+    document.getElementsByClassName('cardhamburguer').style.display = 'none';
+}
