080e6cfc777e50a2aa9927daa21078d11605aee0
Gabriel Novaes <gabrielnovaisoficial@gmail.com>
2023-10-14 03:56:40 -0300

    projeto 5
    adicionado framework (bootstrap), algumas funções em JS para os adicionais dos hamburgueres

--- index.js
+++ index.js
@@ -1,8 +1,75 @@
-function menu(){
-    document.getElementById('menu1').style.backgroundColor = 'red'
+//
 
+
+//adicionais hamburgueres
+
+// chicken
+//mostra/oculta adicionais
+function adict_one() {
+    var display = document.getElementById('chicken').style.display;
+    if(display == "none")
+        document.getElementById('chicken').style.display = 'block';
+    else
+        document.getElementById('chicken').style.display = 'none';
 }
 
-function carhambuerguerid(){
-    document.getElementsByClassName('cardhamburguer').style.display = 'none';
+// la-casa
+//mostra/oculta adicionais
+function adict_two() {
+    var display = document.getElementById('la_casa').style.display;
+    if(display == "none")
+        document.getElementById('la_casa').style.display = 'block';
+    else
+        document.getElementById('la_casa').style.display = 'none';
 }
+
+//nachos
+//mostra/oculta adicionais
+function adict_tree() {
+    var display = document.getElementById('nachos').style.display;
+    if(display == "none")
+        document.getElementById('nachos').style.display = 'block';
+    else
+        document.getElementById('nachos').style.display = 'none';
+}
+
+//smash
+//mostra/oculta adicionais
+function adict_for() {
+    var display = document.getElementById('smash').style.display;
+    if(display == "none")
+        document.getElementById('smash').style.display = 'block';
+    else
+        document.getElementById('smash').style.display = 'none';
+}
+
+//smash II
+//mostra/oculta adicionais
+function adict_five() {
+    var display = document.getElementById('smashII').style.display;
+    if(display == "none")
+        document.getElementById('smashII').style.display = 'block';
+    else
+        document.getElementById('smashII').style.display = 'none';
+}
+//picanha
+//mostra/oculta adicionais
+function adict_six() {
+    var display = document.getElementById('picanha').style.display;
+    if(display == "none")
+        document.getElementById('picanha').style.display = 'block';
+    else
+        document.getElementById('picanha').style.display = 'none';
+}
+//picanhaII
+//mostra/oculta adicionais
+function adict_seven() {
+    var display = document.getElementById('picanhaII').style.display;
+    if(display == "none")
+        document.getElementById('picanhaII').style.display = 'block';
+    else
+        document.getElementById('picanhaII').style.display = 'none';
+}
+
+
+// adicionais porções
